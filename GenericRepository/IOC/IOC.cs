de52186b75e0b7274b01a8df218d5d063cec17a2
Pedro Octávio <pedrooctavio.dev@outlook.com>
2022-01-14 14:38:31 -0300

    IOC created

---
+++ GenericRepository/IOC/IOC.cs
@@ -0,0 +1,12 @@
+﻿using GenericRepository.Implementations;
+using GenericRepository.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GenericRepository.IOC
+{
+    public static class IOC
+    {
+        public static void RegisterGenericRepository(this IServiceCollection services) =>
+            services.AddTransient<IRepository, Repository>();
+    }
+}
