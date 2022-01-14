4c8347abc5231a961ea9eb56c1983219eda0f9e2
Pedro Octávio <pedrooctavio.dev@outlook.com>
2022-01-14 13:39:50 -0300

    IGenericRepository created

---
+++ GenericRepository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericRepository.Interfaces
+{
+    public interface IGenericRepository
+    {
+        Task<IEnumerable<T>> GetAllAsync<T>(CancellationToken cancellationToken);
+        IEnumerable<T> GetAll<T>();
+        Task<T> GetByIdAsync<T, U>(U id, CancellationToken cancellationToken);
+        T GetById<T, U>(U id);
+        Task AddAsync<T>(T entity, CancellationToken cancellationToken);
+        void Add<T>(T entity);
+        void Update<T>(T entity);
+        void Delete<T>(T entity);
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+        void SaveChanges();
+    }
+}
