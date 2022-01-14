52a468f100ea5d78fcde134e77cabd33c0ffb853
Pedro Octávio <pedrooctavio.dev@outlook.com>
2022-01-14 14:31:58 -0300

    GenericRepository created

---
+++ GenericRepository/Implementations/GenericRepository.cs
@@ -0,0 +1,79 @@
+﻿using GenericRepository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericRepository.Implementations
+{
+    internal class GenericRepository : IGenericRepository
+    {
+        public GenericRepository(DbContext dbContext) =>
+            _dbContext = dbContext;
+
+        private readonly DbContext _dbContext;
+
+        public async Task<IEnumerable<T>> ToListAsync<T>(CancellationToken cancellationToken) where T : class =>
+            await _dbContext
+                .Set<T>()
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+        public IEnumerable<T> ToList<T>() where T : class =>
+            _dbContext
+                .Set<T>()
+                .AsNoTracking()
+                .ToList();
+
+        public async Task<T> FindAsync<T, U>(U id, CancellationToken cancellationToken) where T : class
+        {
+            var entity = await _dbContext
+                .Set<T>()
+                .FindAsync(new object[] { id }, cancellationToken);
+
+            if (entity != null)
+                _dbContext.Entry(entity).State = EntityState.Detached;
+
+            return entity;
+        }
+
+        public T Find<T, U>(U id) where T : class
+        {
+            var entity = _dbContext
+                .Set<T>()
+                .Find(id);
+
+            if (entity != null)
+                _dbContext.Entry(entity).State = EntityState.Detached;
+
+            return entity;
+        }
+
+        public async Task AddAsync<T>(T entity, CancellationToken cancellationToken) where T : class =>
+            await _dbContext
+                .Set<T>()
+                .AddAsync(entity, cancellationToken);
+
+        public void Add<T>(T entity) where T : class =>
+            _dbContext
+                .Set<T>()
+                .Add(entity);
+
+        public void Update<T>(T entity) where T : class =>
+            _dbContext
+                .Set<T>()
+                .Update(entity);
+
+        public void Remove<T>(T entity) where T : class =>
+            _dbContext
+                .Set<T>()
+                .Remove(entity);
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken) =>
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+        public void SaveChanges() =>
+            _dbContext.SaveChanges();
+    }
+}
