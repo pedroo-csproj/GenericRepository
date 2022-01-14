3b44d2e1c442f80a64f64f7a0f936c9c64fc7501
Pedro Octávio <pedrooctavio.dev@outlook.com>
2022-01-14 14:18:36 -0300

    name of functions adaped

--- GenericRepository/Interfaces/IGenericRepository.cs
+++ GenericRepository/Interfaces/IGenericRepository.cs
@@ -6,14 +6,14 @@ namespace GenericRepository.Interfaces
 {
     public interface IGenericRepository
     {
-        Task<IEnumerable<T>> GetAllAsync<T>(CancellationToken cancellationToken);
-        IEnumerable<T> GetAll<T>();
-        Task<T> GetByIdAsync<T, U>(U id, CancellationToken cancellationToken);
-        T GetById<T, U>(U id);
-        Task AddAsync<T>(T entity, CancellationToken cancellationToken);
-        void Add<T>(T entity);
-        void Update<T>(T entity);
-        void Delete<T>(T entity);
+        Task<IEnumerable<T>> ToListAsync<T>(CancellationToken cancellationToken) where T : class;
+        IEnumerable<T> ToList<T>() where T : class;
+        Task<T> FindAsync<T, U>(U id, CancellationToken cancellationToken) where T : class;
+        T Find<T, U>(U id) where T : class;
+        Task AddAsync<T>(T entity, CancellationToken cancellationToken) where T : class;
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Remove<T>(T entity) where T : class;
         Task SaveChangesAsync(CancellationToken cancellationToken);
         void SaveChanges();
     }
