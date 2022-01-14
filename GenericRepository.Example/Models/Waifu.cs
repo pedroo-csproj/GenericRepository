d35c3a093a2dc6dca033ad51b6515ec21f9a1e0c
Pedro Octávio <pedrooctavio.dev@outlook.com>
2022-01-14 14:47:47 -0300

    Waifu created

---
+++ GenericRepository.Example/Models/Waifu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GenericRepository.Example.Models
+{
+    public class Waifu
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Uri URL { get; set; }
+    }
+}
