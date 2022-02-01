43beefcd9aeabb1db72062c94991a8069e3a7a71
pedro.octavio <pedrooctavio.dev@outlook.com>
2022-01-31 21:00:35 -0300

    repositories with explicit domain entity created

--- GenericRepository/Implementations/Repository.cs
+++ GenericRepository/Implementations/Repository.cs
@@ -7,6 +7,78 @@ using System.Threading.Tasks;
 
 namespace GenericRepository.Implementations
 {
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        public Repository(DbContext dbContext) =>
+            _dbContext = dbContext;
+
+        private readonly DbContext _dbContext;
+
+        public async Task<IEnumerable<T>> ToListAsync(int quantity = 10, CancellationToken cancellationToken = default) =>
+            await _dbContext
+                .Set<T>()
+                .AsNoTracking()
+                .Take(quantity)
+                .ToListAsync(cancellationToken);
+
+        public IEnumerable<T> ToList(int quantity = 10) =>
+            _dbContext
+                .Set<T>()
+                .AsNoTracking()
+                .Take(quantity)
+                .ToList();
+
+        public async Task<T> FindAsync<U>(U id, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext
+                .Set<T>()
+                .FindAsync(new object[] { id }, cancellationToken);
+
+            if (entity != null)
+                _dbContext.Entry(entity).State = EntityState.Detached;
+
+            return entity;
+        }
+
+        public T Find<U>(U id)
+        {
+            var entity = _dbContext
+                .Set<T>()
+                .Find(id);
+
+            if (entity != null)
+                _dbContext.Entry(entity).State = EntityState.Detached;
+
+            return entity;
+        }
+
+        public async Task AddAsync(T entity, CancellationToken cancellationToken) =>
+            await _dbContext
+                .Set<T>()
+                .AddAsync(entity, cancellationToken);
+
+        public void Add(T entity) =>
+            _dbContext
+                .Set<T>()
+                .Add(entity);
+
+        public void Update(T entity) =>
+            _dbContext
+                .Set<T>()
+                .Update(entity);
+
+        public void Remove(T entity) =>
+            _dbContext
+                .Set<T>()
+                .Remove(entity);
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken) =>
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+        public void SaveChanges() =>
+            _dbContext.SaveChanges();
+    }
+
     public abstract class Repository : IRepository
     {
         public Repository(DbContext dbContext) =>
--- GenericRepository/Interfaces/IRepository.cs
+++ GenericRepository/Interfaces/IRepository.cs
@@ -5,6 +5,80 @@ using System.Threading.Tasks;
 namespace GenericRepository.Interfaces
 {
     /// <summary>
+    /// interface that provides useful methods for data access with explicit domain entity
+    /// </summary>
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// lists a table corresponding to the provided entity limited by the quantity as asynchronous
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <param name="quantity">quantity to limit results</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>returns a list of corresponding entity limited by the provided quantity</returns>
+        Task<IEnumerable<T>> ToListAsync(int quantity = 10, CancellationToken cancellationToken = new CancellationToken());
+        /// <summary>
+        /// lists a table corresponding to the provided entity limited by the quantity
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <param name="quantity">quantity to limit results</param>
+        /// <returns>returns a list of corresponding entity limited by the provided quantity</returns>
+        IEnumerable<T> ToList(int quantity = 10);
+        /// <summary>
+        /// gets the row of the table that primary key matches to the provided id as asynchronous
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <typeparam name="U">type of primary key</typeparam>
+        /// <param name="id">primary key to search in table</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>returns a corresponding entity</returns>
+        Task<T> FindAsync<U>(U id, CancellationToken cancellationToken);
+        /// <summary>
+        /// gets the row of the table that primary key matches to the provided id
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <typeparam name="U">type of primary key</typeparam>
+        /// <param name="id">primary key to search in table</param>
+        /// <returns>returns a corresponding entity<</returns>
+        T Find<U>(U id);
+        /// <summary>
+        /// add the provided entity to the table as asynchronous
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <param name="entity">entity that will be added to the table</param>
+        /// <param name="cancellationToken"></param>
+        Task AddAsync(T entity, CancellationToken cancellationToken);
+        /// <summary>
+        /// add the provided entity to the table
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <param name="entity">entity that will be added to the table</param>
+        void Add(T entity);
+        /// <summary>
+        /// updates a entity in the table
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <param name="entity">entity that will be updated in the table</param>
+        void Update(T entity);
+        /// <summary>
+        /// deletes a entity in the table
+        /// </summary>
+        /// <typeparam name="T">entity corresponding to table</typeparam>
+        /// <param name="entity">entity that will be deleted from the table</param>
+        void Remove(T entity);
+        /// <summary>
+        /// save all changes as asynchronous
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+        /// <summary>
+        /// save all changes
+        /// </summary>
+        void SaveChanges();
+    }
+
+
+    /// <summary>
     /// interface that provides useful methods for data access
     /// </summary>
     public interface IRepository
